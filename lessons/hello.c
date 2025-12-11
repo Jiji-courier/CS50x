b3283703218d1003f8c5daebec55faee0a9b2ccc
Kelly Huang <102801642+Jiji-courier@users.noreply.github.com>
2025-12-11 16:33:06 +1100

    Changed from CS50x language to standard c
    Updated the code to use fgets for input and removed cs50.h.

--- lessons/hello.c
+++ lessons/hello.c
@@ -1,15 +1,27 @@
-#include <cs50.h>
 #include <stdio.h>
 /*.h means header file, needed to access library*/
+
 int main(void)
 {
-    //printf("hello, \"world\" \n");//
-/*semicolons are the equivalent of full stops*/
-    string answer = get_string("What's your name? ");
+    char answer[100];
+    printf("What's your name? ");
+    fgets(answer, 100, stdin);
+    /* fgets = File Get String. stdin is short for Standard Input ie. keyboard */
     printf("hello, %s\n", answer);
+
+    return 0;
+    /* send an exit status code back to the OS to indicate how the program finished. 0 is success. No need for else statement as this program isn't designed to have failure*/
 }
 
-/* Running the code requires
+/* Running the code requires(clang)
+    1) clang -o hello hello.c
+    ie. name the resulting executable file hello. given the source file hello.c
+    2) ./hello
+    */
+
+
+
+/* Running the code requires(cs50)
     1) make hello ie. compiles source code to machine code
     2) ./hello ie. run as the dot means current folder
 
